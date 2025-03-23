ea291622cb7473c82bbf46e8b1551ea158839ee9
Дмитрий Наумов <dmi3naumov@gmail.com>
2025-03-24 02:02:05 +0300

    homework 14.1
    (created Product & Category classes + tests)

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,11 +1,13 @@
 import pytest
-from src.utils import Product
-from src.utils import Category
+
+from src.utils import Category, Product
 
 
 def test_product_initialization():
     # Проверка инициализации объекта класса Product
-    product = Product(name="Ноутбук", description="Игровой ноутбук", price=1200.00, quantity=5)
+    product = Product(
+        name="Ноутбук", description="Игровой ноутбук", price=1200.00, quantity=5
+    )
 
     assert product.name == "Ноутбук"
     assert product.description == "Игровой ноутбук"
@@ -15,9 +17,17 @@ def test_product_initialization():
 
 def test_category_initialization():
     # Проверка инициализации объекта класса Category
-    product1 = Product(name="Ноутбук", description="Игровой ноутбук", price=1200.00, quantity=5)
-    product2 = Product(name="Мышь", description="Беспроводная мышь", price=50.00, quantity=10)
-    category = Category(name="Гаджеты", description="Электроприборы и техника", products=[product1, product2])
+    product1 = Product(
+        name="Ноутбук", description="Игровой ноутбук", price=1200.00, quantity=5
+    )
+    product2 = Product(
+        name="Мышь", description="Беспроводная мышь", price=50.00, quantity=10
+    )
+    category = Category(
+        name="Гаджеты",
+        description="Электроприборы и техника",
+        products=[product1, product2],
+    )
 
     assert category.name == "Гаджеты"
     assert category.description == "Электроприборы и техника"
@@ -30,11 +40,21 @@ def test_total_categories():
     # Проверка правильности подсчета категорий
     Category.total_categories = 0  # обнуляем перед тестом
 
-    product1 = Product(name="Ноутбук", description="Игровой ноутбук", price=1200.00, quantity=5)
-    product2 = Product(name="Мышь", description="Беспроводная мышь", price=50.00, quantity=10)
+    product1 = Product(
+        name="Ноутбук", description="Игровой ноутбук", price=1200.00, quantity=5
+    )
+    product2 = Product(
+        name="Мышь", description="Беспроводная мышь", price=50.00, quantity=10
+    )
 
-    category1 = Category(name="Гаджеты", description="Электроприборы и техника", products=[product1, product2])
-    category2 = Category(name="Одежда", description="Мужская и женская одежда", products=[product1])
+    category1 = Category(
+        name="Гаджеты",
+        description="Электроприборы и техника",
+        products=[product1, product2],
+    )
+    category2 = Category(
+        name="Одежда", description="Мужская и женская одежда", products=[product1]
+    )
 
     assert Category.total_categories == 2
 
@@ -43,10 +63,22 @@ def test_total_products():
     # Проверка правильности подсчета продуктов
     Category.total_products = 0  # обнуляем перед тестом
 
-    product1 = Product(name="Ноутбук", description="Игровой ноутбук", price=1200.00, quantity=5)
-    product2 = Product(name="Мышь", description="Беспроводная мышь", price=50.00, quantity=10)
+    product1 = Product(
+        name="Ноутбук", description="Игровой ноутбук", price=1200.00, quantity=5
+    )
+    product2 = Product(
+        name="Мышь", description="Беспроводная мышь", price=50.00, quantity=10
+    )
 
-    category1 = Category(name="Гаджеты", description="Электроприборы и техника", products=[product1, product2])
-    category2 = Category(name="Одежда", description="Мужская и женская одежда", products=[product1])
+    category1 = Category(
+        name="Гаджеты",
+        description="Электроприборы и техника",
+        products=[product1, product2],
+    )
+    category2 = Category(
+        name="Одежда", description="Мужская и женская одежда", products=[product1]
+    )
 
-    assert Category.total_products == 3  # всего 3 продукта: 2 из категории "Electronics" и 1 из "Clothing"
+    assert (
+        Category.total_products == 3
+    )  # всего 3 продукта: 2 из категории "Electronics" и 1 из "Clothing"
